aa3a69753a6f92f2d89be1775f4b645a0ffe1221
vandanacr538 <vandanacr538@gmail.com>
2024-08-08 23:54:57 +0530

    getElementsByClassName Method-Task

--- DOM/index.js
+++ DOM/index.js
@@ -1,14 +1,24 @@
-const heading=document.getElementById("main-heading");
-const header=document.getElementById("header");
-const basketHeading=document.getElementById("basket-heading");
-const thanksDiv=document.getElementById("thanks");
+// Task 1 code
+// const heading=document.getElementById("main-heading");
+// const header=document.getElementById("header");
+// const basketHeading=document.getElementById("basket-heading");
+// const thanksDiv=document.getElementById("thanks");
 
-heading.textContent= "Fruit World";
-heading.style.color="orange";
+// heading.textContent= "Fruit World";
+// heading.style.color="orange";
 
-header.style.backgroundColor="green";
-header.style.borderBottom="2px solid orange";
+// header.style.backgroundColor="green";
+// header.style.borderBottom="2px solid orange";
 
-basketHeading.style.color="green";
+// basketHeading.style.color="green";
 
-thanksDiv.innerHTML='<p>Please visit us again</p>'
+// thanksDiv.innerHTML='<p>Please visit us again</p>'
+
+// Task 2 Code
+const fruit=document.getElementsByClassName("fruit");
+
+fruit[2].style.backgroundColor='yellow';
+
+for(let i=0;i<fruit.length;i++){
+  fruit[i].style.fontWeight="bold";
+}
