6928f2a434955dba803d40b900b4a04dd98fd696
vandanacr538 <vandanacr538@gmail.com>
2024-08-08 23:32:53 +0530

    Understanding DOM & getElementById Method-Task

---
+++ DOM/index.js
@@ -0,0 +1,14 @@
+const heading=document.getElementById("main-heading");
+const header=document.getElementById("header");
+const basketHeading=document.getElementById("basket-heading");
+const thanksDiv=document.getElementById("thanks");
+
+heading.textContent= "Fruit World";
+heading.style.color="orange";
+
+header.style.backgroundColor="green";
+header.style.borderBottom="2px solid orange";
+
+basketHeading.style.color="green";
+
+thanksDiv.innerHTML='<p>Please visit us again</p>'
